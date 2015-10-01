9252738c39d5a35aa3063d34b637f1c8f9b8f692
Kevin White <kbw612@yahoo.com>
2015-10-01 10:40:14 -0400

    Initial code setup and first failed test

---
+++ BabysitterKata/Babysitter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabysitterKata
+{
+    public class Babysitter
+    {
+        public int CalculatePay(DateTime startTime, DateTime endTime)
+        {
+            return 0;
+        }
+    }
+}
---
+++ BabysitterTest/BabysitterTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BabysitterKata;
+
+namespace BabysitterTest
+{
+    [TestClass]
+    public class BabysitterTest
+    {
+        [TestMethod]
+        public void WhenBabysitterWorksUntilBedtime()
+        {
+            // Arrange
+            Babysitter babysitter = new Babysitter();
+            DateTime startTime = new DateTime(2015, 9, 29, 17, 0, 0);
+            DateTime endTime = new DateTime(2015, 9, 29, 21, 0, 0);
+
+            // Act
+            int pay = babysitter.CalculatePay(startTime, endTime);
+
+            // Assert
+            Assert.AreEqual(48, pay);
+        }
+    }
+}
